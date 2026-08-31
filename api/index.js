1b866e3430e2609608452d50ddaa29f834dd31f8
eminememis953-cmd <eminememis953@gmail.com>
2026-08-31 18:28:03 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
+   
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Sadece POST isteği kabul edilir."
@@ -6,13 +15,16 @@ export default async function handler(req, res) {
   }
 
   try {
-    const { image, category } = req.body || {};
+    const { image, category } = req.body;
 
     if (!image) {
       return res.status(400).json({
         error: "Fotoğraf bulunamadı."
       });
     }
+  
+      
+    
 
     if (!process.env.GEMINI_API_KEY) {
       return res.status(500).json({
